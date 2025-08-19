4ef5067360debe7e696da603bd7ea0dffa2a88c5
Elijah Wright <git@elijahs.space>
2025-08-18 17:50:55 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# blog.elijahs.space
---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'blog.elijahs.space';
+export const SITE_DESCRIPTION = 'the elijahs.space blog';
